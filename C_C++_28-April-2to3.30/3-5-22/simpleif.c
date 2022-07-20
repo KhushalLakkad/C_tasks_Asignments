7b0e264a79271a506d59be152e8e386aa3c2b115
Deep21Patel <deeppatel04881@gmail.com>
2022-07-20 16:30:53 +0530

    C language

---
+++ C_C++_28-April-2to3.30/3-5-22/simpleif.c
@@ -0,0 +1,13 @@
+Conditional Statement: To make decision.
+
+1) Simple if
+2) if/else
+3) Nested if/else
+4) Ladder if/else
+
+1) Simple if :  Syntax :   if(condition)
+			   {
+				Statement;
+			   }
+
+
---
+++ nested_if.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a=67,b=7,c=90;
+	
+	return 0;
+}
